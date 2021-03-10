e0aeb6f41b955a60fd9b7e803875c3acfd870650
username <user@email.mail>
2021-03-10 17:19:38 +0900

    ma

--- memory/memory4x4.js
+++ memory/memory4x4.js
@@ -7,6 +7,13 @@ const bt6 = document.querySelector('.btn6');
 const bt7 = document.querySelector('.btn7');
 const bt8 = document.querySelector('.btn8');
 const bt9 = document.querySelector('.btn9');
+const bt10 = document.querySelector('.btn10');
+const bt11 = document.querySelector('.btn11');
+const bt12 = document.querySelector('.btn12');
+const bt13 = document.querySelector('.btn13');
+const bt14 = document.querySelector('.btn14');
+const bt15 = document.querySelector('.btn15');
+const bt16 = document.querySelector('.btn16');
 const score = document.getElementById('score');
 const stbtn = document.querySelector('.startbtn');
 
@@ -21,7 +28,7 @@ var arr = [];
 var arrcheak = [];
 var point;
 
-for (var i = 0; i < 9; i++) {
+for (var i = 0; i < 12; i++) {
     var num = randomNum();
     arr.push(num);
 }
@@ -40,55 +47,97 @@ btncolor = function (chois) {
             bt1.style.backgroundColor = 'red';
             timeid = setTimeout(function () {
                 bt1.style.backgroundColor = '#b4fbff';
-            }, 900);
+            }, 750);
             break;
         case 1:
             bt2.style.backgroundColor = 'red';
             timeid = setTimeout(function () {
                 bt2.style.backgroundColor = '#b4fbff';
-            }, 900);
+            }, 750);
             break;
         case 2:
             bt3.style.backgroundColor = 'red';
             timeid = setTimeout(function () {
                 bt3.style.backgroundColor = '#b4fbff';
-            }, 900);
+            }, 750);
             break;
         case 3:
             bt4.style.backgroundColor = 'red';
             timeid = setTimeout(function () {
                 bt4.style.backgroundColor = '#b4fbff';
-            }, 900);
+            }, 750);
             break;
         case 4:
             bt5.style.backgroundColor = 'red';
             timeid = setTimeout(function () {
                 bt5.style.backgroundColor = '#b4fbff';
-            }, 900);
+            }, 750);
             break;
         case 5:
             bt6.style.backgroundColor = 'red';
             timeid = setTimeout(function () {
                 bt6.style.backgroundColor = '#b4fbff';
-            }, 900);
+            }, 750);
             break;
         case 6:
             bt7.style.backgroundColor = 'red';
             timeid = setTimeout(function () {
                 bt7.style.backgroundColor = '#b4fbff';
-            }, 900);
+            }, 750);
             break;
         case 7:
             bt8.style.backgroundColor = 'red';
             timeid = setTimeout(function () {
                 bt8.style.backgroundColor = '#b4fbff';
-            }, 900);
+            }, 750);
             break;
         case 8:
             bt9.style.backgroundColor = 'red';
             timeid = setTimeout(function () {
                 bt9.style.backgroundColor = '#b4fbff';
-            }, 900);
+            }, 750);
+            break;
+        case 9:
+            bt9.style.backgroundColor = 'red';
+            timeid = setTimeout(function () {
+                bt9.style.backgroundColor = '#b4fbff';
+            }, 750);
+            break;
+        case 10:
+            bt9.style.backgroundColor = 'red';
+            timeid = setTimeout(function () {
+                bt9.style.backgroundColor = '#b4fbff';
+            }, 750);
+            break;
+        case 11:
+            bt9.style.backgroundColor = 'red';
+            timeid = setTimeout(function () {
+                bt9.style.backgroundColor = '#b4fbff';
+            }, 750);
+            break;
+        case 12:
+            bt9.style.backgroundColor = 'red';
+            timeid = setTimeout(function () {
+                bt9.style.backgroundColor = '#b4fbff';
+            }, 750);
+            break;
+        case 13:
+            bt9.style.backgroundColor = 'red';
+            timeid = setTimeout(function () {
+                bt9.style.backgroundColor = '#b4fbff';
+            }, 750);
+            break;
+        case 14:
+            bt9.style.backgroundColor = 'red';
+            timeid = setTimeout(function () {
+                bt9.style.backgroundColor = '#b4fbff';
+            }, 750);
+            break;
+        case 15:
+            bt9.style.backgroundColor = 'red';
+            timeid = setTimeout(function () {
+                bt9.style.backgroundColor = '#b4fbff';
+            }, 750);
             break;
     }
 };
@@ -99,9 +148,9 @@ bt1.addEventListener('click', function () {
         j++;
 
         point = Number(j).toLocaleString('en');
-        score.innerText = '현재점수 : ' + point + ' / 9';
+        score.innerText = '현재점수 : ' + point + ' / 12';
     } else {
-        score.innerText = '최종점수 : ' + point + ' / 9';
+        score.innerText = '최종점수 : ' + point + ' / 12';
         j = 0;
     }
 });
@@ -111,9 +160,9 @@ bt2.addEventListener('click', function () {
         j++;
 
         point = Number(j).toLocaleString('en');
-        score.innerText = '현재점수 : ' + point + ' / 9';
+        score.innerText = '현재점수 : ' + point + ' / 12';
     } else {
-        score.innerText = '최종점수 : ' + point + ' / 9';
+        score.innerText = '최종점수 : ' + point + ' / 12';
         j = 0;
     }
 });
@@ -123,9 +172,9 @@ bt3.addEventListener('click', function () {
         j++;
 
         point = Number(j).toLocaleString('en');
-        score.innerText = '현재점수 : ' + point + ' / 9';
+        score.innerText = '현재점수 : ' + point + ' / 12';
     } else {
-        score.innerText = '최종점수 : ' + point + ' / 9';
+        score.innerText = '최종점수 : ' + point + ' / 12';
         j = 0;
     }
 });
@@ -135,9 +184,9 @@ bt4.addEventListener('click', function () {
         j++;
 
         point = Number(j).toLocaleString('en');
-        score.innerText = '현재점수 : ' + point + ' / 9';
+        score.innerText = '현재점수 : ' + point + ' / 12';
     } else {
-        score.innerText = '최종점수 : ' + point + ' / 9';
+        score.innerText = '최종점수 : ' + point + ' / 12';
         j = 0;
     }
 });
@@ -147,9 +196,9 @@ bt5.addEventListener('click', function () {
         j++;
 
         point = Number(j).toLocaleString('en');
-        score.innerText = '현재점수 : ' + point + ' / 9';
+        score.innerText = '현재점수 : ' + point + ' / 12';
     } else {
-        score.innerText = '최종점수 : ' + point + ' / 9';
+        score.innerText = '최종점수 : ' + point + ' / 12';
         j = 0;
     }
 });
@@ -159,9 +208,9 @@ bt6.addEventListener('click', function () {
         j++;
 
         point = Number(j).toLocaleString('en');
-        score.innerText = '현재점수 : ' + point + ' / 9';
+        score.innerText = '현재점수 : ' + point + ' / 12';
     } else {
-        score.innerText = '최종점수 : ' + point + ' / 9';
+        score.innerText = '최종점수 : ' + point + ' / 12';
         j = 0;
     }
 });
@@ -170,9 +219,9 @@ bt7.addEventListener('click', function () {
     if (a === 6) {
         j++;
         point = Number(j).toLocaleString('en');
-        score.innerText = '현재점수 : ' + point + ' / 9';
+        score.innerText = '현재점수 : ' + point + ' / 12';
     } else {
-        score.innerText = '최종점수 : ' + point + ' / 9';
+        score.innerText = '최종점수 : ' + point + ' / 12';
         j = 0;
     }
 });
@@ -181,9 +230,9 @@ bt8.addEventListener('click', function () {
     if (a === 7) {
         j++;
         point = Number(j).toLocaleString('en');
-        score.innerText = '현재점수 : ' + point + ' / 9';
+        score.innerText = '현재점수 : ' + point + ' / 12';
     } else {
-        score.innerText = '최종점수 : ' + point + ' / 9';
+        score.innerText = '최종점수 : ' + point + ' / 12';
         j = 0;
     }
 });
@@ -192,9 +241,9 @@ bt9.addEventListener('click', function () {
     if (a === 8) {
         j++;
         point = Number(j).toLocaleString('en');
-        score.innerText = '현재점수 : ' + point + ' / 9';
+        score.innerText = '현재점수 : ' + point + ' / 12';
     } else {
-        score.innerText = '최종점수 : ' + point + ' / 9';
+        score.innerText = '최종점수 : ' + point + ' / 12';
         j = 0;
     }
 });
@@ -202,13 +251,13 @@ bt9.addEventListener('click', function () {
 stbtn.addEventListener('click', function () {
     i = 0;
     timeid2 = setInterval(function () {
-        score.innerText = '현재점수 : 0 / 9';
+        score.innerText = '현재점수 : 0 / 12';
         var c = arr[i];
-        if (i === 8) {
+        if (i === 11) {
             clearInterval(timeid2);
             i = 0;
         }
         btncolor(c);
         i++;
-    }, 1000);
+    }, 850);
 });
